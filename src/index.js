463a0f666a2ed8001da509ec7cf4378acbdca788
jack13only <jack13only@gmail.com>
2021-08-02 19:00:45 +0300

    feat: add better solution

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 module.exports = function reverse (n) {
-    if (n<0) {
-        return +Math.abs(n)
-                      .toString()
-                      .split('')
-                      .reverse()
-                      .join('');
-    }
-    return +n.toString()
-             .split('')
-             .reverse()
-             .join('');
+    return +Math.abs(n)
+                .toString()
+                .split('')
+                .reverse()
+                .join('');
 }
