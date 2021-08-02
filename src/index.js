4298e889958a4c4a03bb775a60466ea5cc0fd3d4
jack13only <jack13only@gmail.com>
2021-08-02 18:58:54 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if (n<0) {
+        return +Math.abs(n)
+                      .toString()
+                      .split('')
+                      .reverse()
+                      .join('');
+    }
+    return +n.toString()
+             .split('')
+             .reverse()
+             .join('');
 }
